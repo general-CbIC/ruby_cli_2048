2f0c278656ceb1a6b051edffcb5c4eeb101ef995
Alexandr Sysoev <sanchous.ok@hotmail.com>
2017-07-19 14:58:13 +0300

    0.0.4
    missed file

--- game.rb
+++ game.rb
@@ -1,17 +1,37 @@
 require './bin/game'
 
+VERSION = '0.0.4'
+
 def show_menu
   system 'clear'
   puts 'Choose variant:'
   puts '1. Start game'
+  puts '2. Help'
   puts '0. Exit'
   answer = gets.chomp
-  menu if answer.to_i > 1 || answer.to_i < 0
+  show_menu if answer.to_i > 2 || answer.to_i < 0
   if answer == '1'
     game = Game.new
     game.start_game
   end
+  show_help if answer == '2'
   exit if answer == '0'
+end
+
+def show_help
+  system 'clear'
+  puts "Welcome to 2048 CLI (#{VERSION})"
+  puts '___________________________'
+  puts 'Rules and info about game you can read here:'
+  puts 'https://en.wikipedia.org/wiki/2048_(video_game)'
+  puts '___________________________'
+  puts 'CONTROL:'
+  puts "'w-a-s-d'\tto move blocks (need to press ENTER after it)"
+  puts "'Esc'\t\tto exit"
+  puts '___________________________'
+  puts '(Press ENTER to exit from help)'
+  gets
+  show_menu
 end
 
 show_menu
