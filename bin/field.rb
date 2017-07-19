6f5ee3a08829ddf9154ecccf04da9f2227ca4d79
Alexandr Sysoev <sanchous.ok@hotmail.com>
2017-07-19 14:57:46 +0300

    0.0.4
    * Beautify field
    * Pre-made control functions
    * Show Score
    * Add help

--- bin/field.rb
+++ bin/field.rb
@@ -1,5 +1,6 @@
 class Field
-  def initialize
+  def initialize(game)
+    @game = game
     @field = [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
@@ -10,15 +11,31 @@ class Field
   end
 
   def show_field
-    print " _______________________________\n"
+    print " ---------------------------------------------------------------\n"
     4.times do |row|
       print "|\t"
       4.times do |col|
         print "#{@field[row][col]}\t|\t"
       end
       print "\n"
-      print " -------------------------------\n"
+      print " ---------------------------------------------------------------\n"
     end
+  end
+
+  def move_up
+    puts 'move up'
+  end
+
+  def move_down
+    puts 'move down'
+  end
+
+  def move_left
+    puts 'move left'
+  end
+
+  def move_right
+    puts 'move right'
   end
 
   private
@@ -47,12 +64,12 @@ class Field
     if empty_blocks.empty?
       game_over
     else
-      block = random_empty_block
-      @field[block[0]][block[1]] = rand(2).zero? ? 2 : 4
+      row, col = random_empty_block
+      @field[row][col] = rand(2).zero? ? 2 : 4
     end
   end
 
   def game_over
-    # game over
+    @game.game_over
   end
 end
--- bin/game.rb
+++ bin/game.rb
@@ -1,11 +1,43 @@
 class Game
   require './bin/field'
 
+  attr_accessor :score
+
   def initialize
-    @field = Field.new
+    @field = Field.new(self)
+    @score = 0
   end
 
   def start_game
+    update_field
+    wait_for_turn
+  end
+
+  def game_over
+    puts 'Game Over'
+    exit
+  end
+
+  private
+
+  def update_field
+    puts "Score: #{@score}"
     @field.show_field
+    wait_for_turn
+  end
+
+  def wait_for_turn
+    c = gets.chomp
+    case c
+    when 'w', 'W'
+      @field.move_up
+    when 's', 'S'
+      @field.move_down
+    when 'a', 'A'
+      @field.move_left
+    when 'd', 'D'
+      @field.move_right
+    end
+    update_field
   end
 end
